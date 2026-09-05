9a1bc0c6ee289ca4b991c881a015b2be624c2009
ajinkya <salunkheajinkya07@gmail.com>
2026-09-05 09:37:55 +0530

    Add routes, controller, and Jbuilder views for Course-Tutor JSON API

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  scope :api do
+    resources :courses, only: [:index, :create], controller: 'courses'
+  end
 end
